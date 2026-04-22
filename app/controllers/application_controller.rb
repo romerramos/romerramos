38f2405f05e4ffb47cfcf016a36e64e5ca59f7fa
Romer Ramos <romerramos@gmail.com>
2026-04-22 10:54:42 +0200

    Add es/en locales

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,18 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  before_action :set_locale
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
+  private
+
+  def set_locale
+    locale = params[:locale] || I18n.default_locale
+    locale = I18n.default_locale.to_s unless I18n.available_locales.include?(locale.to_sym)
+    I18n.locale = locale
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,11 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  # Defines the root path route ("/")
-  root "home#index"
+  scope "/:locale", locale: /en|es/ do
+    # Defines the root path route ("/")
+    root "home#index"
+  end
+
+  # Redirect root to default locale
+  get "/", to: redirect("/en")
 end
